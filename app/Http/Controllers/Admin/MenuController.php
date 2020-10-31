6bd8cf3d2883231ce63c9a6aa3906cd9630158da
”谷藤諒哉” <uyan1931052@gmail.com>
2020-10-31 02:42:47 +0000

    家計簿

---
+++ app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MenuController extends Controller
+{
+    public function add()
+    {
+        return view('admin.money.nemu');
+    }
+}
